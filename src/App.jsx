1bc5178a43641644427be5ff5cb87a7f7902b02a
Antxon Eguiguren <aeguiguren@hiberus.com>
2024-06-10 00:02:56 +0200

    add Dockerfile

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from 'react';
 
 function App() {
+  const baseUrl =
+    import.meta.env.VITE_REACT_APP_NODE_ENV === 'development'
+      ? import.meta.env.VITE_REACT_APP_LOCAL_BASE_URL
+      : import.meta.env.VITE_REACT_APP_SERVER_BASE_URL;
+
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('http://localhost:3300/api');
+      const response = await fetch(`${baseUrl}/api`);
       const data = await response.json();
       setData(data.message);
     }
     fetchData();
-  }, []);
+  }, [baseUrl]);
 
   const [data, setData] = useState();
 
